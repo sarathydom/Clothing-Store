04a9caa9e3e8a532fdec5aac368dd0e7f84d38c9
parthasarathy <Parthasarath.Purushothaman@tpfsoftware.cony>
2019-12-21 10:01:28 +0530

    Home page added

---
+++ src/Homepage/Homepage.component.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './HomePage.styles.scss'
+
+const HomePage = ()=>(
+        <>
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">jacket</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">glove</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">pant</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+    export default HomePage;
